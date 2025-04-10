a56ea80ffd0b97f077e5e514cda2f96c799832a5
Heitor Vasconcelos <fhv.araujo@gmail.com>
2025-04-10 14:49:51 -0300

    feat: initial project setup and health check route

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"status": "ok",
+		})
+	})
+
+	r.Run(":8080")
+}
